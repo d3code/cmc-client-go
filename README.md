2b9547b5686b79d0d2b2a8ea4c5b3846b2902077
Luke Sands <luke.sands@d3code.com.au>
2022-06-20 13:43:10 +1000

    Update README.md

--- README.md
+++ README.md
@@ -31,3 +31,23 @@ func main() {
     client := cmc.Client("<your-api-key>")
 }
 ```
+
+### Testing
+
+To use the CMC testnet, call `Test(true)` on the client. This will replace the Client ID and the Base URL for the requests to use the sandbox API.
+
+You do not need to provide your Client ID, though this can be provided and calling `Test(false)` will have no effect. This has been done to simplify setting this via config.
+
+```go
+cmc.Client("").Test(true)
+```
+
+### Logging
+
+There is no logging in this library, an `error` is returned if there are request / unmarshalling errors and can be handled accordingly.
+
+To output the raw response's, call `PrintResponse(true)` on the client.
+
+```go
+cmc.Client("").PrintResponse(true)
+```
--- cmc/client.go
+++ cmc/client.go
@@ -1,87 +1,89 @@
 package cmc
 
 import (
-	"io/ioutil"
-	"net/http"
-	"net/url"
-	"time"
+    "io/ioutil"
+    "net/http"
+    "net/url"
+    "time"
 )
 
 const (
-	cmcBaseUrl        = "https://pro-api.coinmarketcap.com"
-	cmcBaseTestnetUrl = "https://sandbox-api.coinmarketcap.com"
+    cmcBaseUrl        = "https://pro-api.coinmarketcap.com"
+    cmcBaseTestnetUrl = "https://sandbox-api.coinmarketcap.com"
 )
 
 type client struct {
-	apiKey        string
-	baseURL       string
-	printResponse bool
+    apiKey        string
+    baseURL       string
+    printResponse bool
 }
 
 func Client(apiKey string) *client {
-	c := &client{
-		apiKey:        apiKey,
-		baseURL:       cmcBaseUrl,
-		printResponse: false,
-	}
+    c := &client{
+        apiKey:        apiKey,
+        baseURL:       cmcBaseUrl,
+        printResponse: false,
+    }
 
-	return c
+    return c
 }
 
 func (c *client) Test(test bool) *client {
-	if test {
-		c.apiKey = "b54bcf4d-1bca-4e8e-9a24-22ff2c3d462c"
-		c.baseURL = cmcBaseTestnetUrl
-	}
+    if test {
+        c.apiKey = "b54bcf4d-1bca-4e8e-9a24-22ff2c3d462c"
+        c.baseURL = cmcBaseTestnetUrl
+    }
 
-	return c
+    return c
 }
 
 func (c *client) PrintResponse(test bool) *client {
-	if test {
-		c.printResponse = true
-	}
+    if test {
+        c.printResponse = true
+        return c
+    }
 
-	return c
+    c.printResponse = false
+    return c
 }
 
 type Status struct {
-	Timestamp    time.Time `json:"timestamp"`
-	ErrorCode    int       `json:"error_code"`
-	ErrorMessage string    `json:"error_message"`
-	Elapsed      int       `json:"elapsed"`
-	CreditCount  int       `json:"credit_count"`
-	Notice       string    `json:"notice"`
+    Timestamp    time.Time `json:"timestamp"`
+    ErrorCode    int       `json:"error_code"`
+    ErrorMessage string    `json:"error_message"`
+    Elapsed      int       `json:"elapsed"`
+    CreditCount  int       `json:"credit_count"`
+    Notice       string    `json:"notice"`
 }
 
 func doGetRequest(requestURL string, q url.Values, c *client) ([]byte, error) {
-	req, err := http.NewRequest(http.MethodGet, requestURL, nil)
-	if err != nil {
-		return nil, err
-	}
+    req, err := http.NewRequest(http.MethodGet, requestURL, nil)
+    if err != nil {
+        return nil, err
+    }
 
-	req.Header.Set("Accepts", "application/json")
-	req.Header.Set("X-CMC_PRO_API_KEY", c.apiKey)
-	req.URL.RawQuery = q.Encode()
+    req.Header.Set("Accepts", "application/json")
+    req.Header.Set("X-CMC_PRO_API_KEY", c.apiKey)
+    req.URL.RawQuery = q.Encode()
 
-	httpClient := http.Client{
-		Timeout: 30 * time.Second,
-	}
+    httpClient := http.Client{
+        Timeout: 30 * time.Second,
+    }
 
-	res, requestError := httpClient.Do(req)
-	if requestError != nil {
-		return nil, requestError
-	}
+    res, requestError := httpClient.Do(req)
+    if requestError != nil {
+        return nil, requestError
+    }
 
-	resBody, responseError := ioutil.ReadAll(res.Body)
-	if responseError != nil {
-		return nil, responseError
-	}
+    resBody, responseError := ioutil.ReadAll(res.Body)
+    if responseError != nil {
+        return nil, responseError
+    }
 
-	if c.printResponse && resBody != nil {
-		responseString := string(resBody)
-		println(responseString)
-	}
+    if c.printResponse && resBody != nil {
+        responseString := string(resBody)
+        println(responseString)
+    }
 
-	return resBody, nil
+    return resBody, nil
 }
