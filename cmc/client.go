85e48ba77d93cb8e44e909469478e443bae422f0
Luke Sands <luke.sands@d3code.com.au>
2022-06-20 14:54:07 +1000

    Update client

--- cmc/client.go
+++ cmc/client.go
@@ -1,95 +1,96 @@
 package cmc
 
 import (
-    "io/ioutil"
-    "net/http"
-    "net/url"
-    "time"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"time"
 )
 
 const (
-    cmcBaseUrl     = "https://pro-api.coinmarketcap.com"
-    cmcBaseTestUrl = "https://sandbox-api.coinmarketcap.com"
-    testApiKey     = "b54bcf4d-1bca-4e8e-9a24-22ff2c3d462c"
+	cmcBaseUrl      = "https://pro-api.coinmarketcap.com"
+	cmcBaseTestUrl  = "https://sandbox-api.coinmarketcap.com"
+	cmcApiKeyHeader = "X-CMC_PRO_API_KEY"
+	testApiKey      = "b54bcf4d-1bca-4e8e-9a24-22ff2c3d462c"
 )
 
 type client struct {
-    apiKey        string
-    test          bool
-    printResponse bool
+	apiKey        string
+	test          bool
+	printResponse bool
 }
 
 func Client(apiKey string) *client {
-    c := &client{
-        apiKey:        apiKey,
-        test:          false,
-        printResponse: false,
-    }
+	c := &client{
+		apiKey:        apiKey,
+		test:          false,
+		printResponse: false,
+	}
 
-    return c
+	return c
 }
 
 func (c *client) Test(test bool) *client {
-    c.test = test
-    return c
+	c.test = test
+	return c
 }
 
 func (c *client) PrintResponse(printResponse bool) *client {
-    c.printResponse = printResponse
-    return c
+	c.printResponse = printResponse
+	return c
 }
 
 type Status struct {
-    Timestamp    time.Time `json:"timestamp"`
-    ErrorCode    int       `json:"error_code"`
-    ErrorMessage string    `json:"error_message"`
-    Elapsed      int       `json:"elapsed"`
-    CreditCount  int       `json:"credit_count"`
-    Notice       string    `json:"notice"`
+	Timestamp    time.Time `json:"timestamp"`
+	ErrorCode    int       `json:"error_code"`
+	ErrorMessage string    `json:"error_message"`
+	Elapsed      int       `json:"elapsed"`
+	CreditCount  int       `json:"credit_count"`
+	Notice       string    `json:"notice"`
 }
 
-func doGetRequest(requestURL string, q url.Values, c *client) ([]byte, error) {
-    var request string
+func doGetRequest(requestUrl string, q url.Values, c *client) ([]byte, error) {
+	var baseUrl string
 
-    if c.test {
-        request = cmcBaseTestUrl + requestURL
-    } else {
-        request = cmcBaseUrl + requestURL
-    }
+	if c.test {
+		baseUrl = cmcBaseTestUrl + requestUrl
+	} else {
+		baseUrl = cmcBaseUrl + requestUrl
+	}
 
-    println("URL: ", request)
-    req, err := http.NewRequest(http.MethodGet, request, nil)
-    if err != nil {
-        return nil, err
-    }
+	req, err := http.NewRequest(http.MethodGet, baseUrl, nil)
+	if err != nil {
+		return nil, err
+	}
 
-    if c.test {
-        req.Header.Set("X-CMC_PRO_API_KEY", testApiKey)
-    } else {
-        req.Header.Set("X-CMC_PRO_API_KEY", c.apiKey)
-    }
+	if c.test {
+		req.Header.Set(cmcApiKeyHeader, testApiKey)
+	} else {
+		req.Header.Set(cmcApiKeyHeader, c.apiKey)
+	}
 
-    req.Header.Set("Accepts", "application/json")
-    req.URL.RawQuery = q.Encode()
+	req.Header.Set("Accepts", "application/json")
+	req.URL.RawQuery = q.Encode()
 
-    httpClient := http.Client{
-        Timeout: 30 * time.Second,
-    }
+	httpClient := http.Client{
+		Timeout: 30 * time.Second,
+	}
 
-    res, requestError := httpClient.Do(req)
-    if requestError != nil {
-        return nil, requestError
-    }
+	res, requestError := httpClient.Do(req)
 
-    resBody, responseError := ioutil.ReadAll(res.Body)
-    if responseError != nil {
-        return nil, responseError
-    }
+	if requestError != nil {
+		return nil, requestError
+	}
 
-    if c.printResponse && resBody != nil {
-        responseString := string(resBody)
-        println("Response: ", responseString)
-    }
+	resBody, responseError := ioutil.ReadAll(res.Body)
+	if responseError != nil {
+		return nil, responseError
+	}
 
-    return resBody, nil
+	if c.printResponse && resBody != nil {
+		responseString := string(resBody)
+		println("Response: ", responseString)
+	}
+
+	return resBody, nil
 }
